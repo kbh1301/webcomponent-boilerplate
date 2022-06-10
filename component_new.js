18996a0ac257a871f63933e8a0ad28c3737eb03d
Kyle Hulvey <83362164+kbh1301@users.noreply.github.com>
2022-06-10 01:43:55 -0500

    Update component_new.js

--- component_new.js
+++ component_new.js
@@ -14,7 +14,7 @@ customElements.define("component-new", class extends HTMLElement {
         const temp = document.createElement('template');
         temp.innerHTML = this.htmlcss();
         if(!this.firstElementChild) {
-            this.prepend(temp.content)
+            this.prepend(temp.content);
             this.script();
         }
     }
